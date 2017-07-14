6784348a8d4595bde3b1bd8093b1f522fbc2bcce
jamesleary96@yahoo.com <jamesleary96@yahoo.com>
2017-07-14 16:44:03 -0500

    Wind up server and static files

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// requires
+var express = require( 'express' );
+var app = express();
+var bodyParser = require( 'body-parser' );
+var path = require( 'path' );
+var port = process.env.PORT || 5000;
+
+// middleware
+app.use( bodyParser.urlencoded( { extended: true } ) );
+
+// static files
+app.get('/*', function (req, res) {
+  var file = req.params[0] || 'views/index.html';
+  res.sendFile(path.join(__dirname, '/public', file));
+});
+
+// spin up server
+app.listen(port, function (req, res) {
+  console.log('Now listening on:', port);
+});
